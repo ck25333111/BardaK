0d83bdfc5317d020a44c2b5d80bce8694a38a2b3
ck25333111 <ck.25333111@gmail.com>
2026-02-13 00:14:57 +0300

    chore(frontend): подключен Tailwind CSS v4 и интеграция с Vite

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const bnt = document.getElementById('bnt')
+// const con = document.getElementById('con')
+
+const el = React.createElement(
+  "button",
+  {
+    className: "btn",
+    type: "submit",
+    tabIndex: 0,
+    onClick: () => console.log("Нажимается наконец-то клиик "),
+  },
+  "Заголовок кнопки"
+);
+
+
+const root = ReactDOM.createRoot(bnt);
+root.render(el);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    
   </StrictMode>,
+
+  
 )
